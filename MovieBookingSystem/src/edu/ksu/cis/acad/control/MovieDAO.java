99d2c12347d6fbabf602969677fc44be5a7cdace
Yusuf <yusufali2205@gmail.com>
2015-05-01 17:49:01 -0500

    completed the movie dao class with all required queries

--- MovieBookingSystem/src/edu/ksu/cis/acad/control/MovieDAO.java
+++ MovieBookingSystem/src/edu/ksu/cis/acad/control/MovieDAO.java
@@ -5,7 +5,9 @@ import edu.ksu.cis.acad.model.Movie;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class MovieDAO {
 
@@ -61,19 +63,23 @@ public class MovieDAO {
         return rows_deleted;
     }
     
-    public int editMovie(String movie_id) {
-    	int rows_deleted = 0;
+    public int editMovie(Movie movie) {
+    	int rows_updated = 0;
     	DatabaseConnect db = new DatabaseConnect();
         try {
 			Connection dbConn = db.openConnection();
 			
-			String query = "DELETE FROM MOVIES "
+			String query = "UPDATE MOVIES SET movie_name=?, release_date=?, genre=?, running=?"
     				+ "WHERE movie_id = ?";
     
-			PreparedStatement delete_movie_ps = dbConn.prepareStatement(query);
-			delete_movie_ps.setString(1, movie_id);
+			PreparedStatement update_movie_ps = dbConn.prepareStatement(query);
+			update_movie_ps.setString(1, movie.getMid());
+			update_movie_ps.setString(2, movie.getMname());
+			update_movie_ps.setDate(3, movie.getRelease_date());
+			update_movie_ps.setBoolean(4, movie.isRunning());
+			update_movie_ps.setString(5, movie.getMid());
 			
-			rows_deleted = delete_movie_ps.executeUpdate();
+			rows_updated = update_movie_ps.executeUpdate();
             
             dbConn.close();
             
@@ -83,7 +89,40 @@ public class MovieDAO {
 			e.printStackTrace();
 		}
         
-        return rows_deleted;
+        return rows_updated;
     }
-
+    
+    public ArrayList<Movie> getAllMovies() {
+    	ArrayList<Movie> movies = new ArrayList<Movie>();
+    	DatabaseConnect db = new DatabaseConnect();
+        try {
+			Connection dbConn = db.openConnection();
+			
+			String query = "SELECT * FROM MOVIE";
+    
+			PreparedStatement update_movie_ps = dbConn.prepareStatement(query);
+			
+			ResultSet rows_selected = update_movie_ps.executeQuery();
+            
+			while ( rows_selected.next() ) {
+				Movie movie = new Movie();
+				movie.setMid(rows_selected.getString(1));
+				movie.setMname(rows_selected.getString(2));
+				movie.setRelease_date(rows_selected.getDate(3));
+				movie.setGenre(rows_selected.getString(4));
+				movie.setRunning(rows_selected.getBoolean(5));
+				movies.add(movie);
+			}
+			
+            dbConn.close();
+            
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        return movies;
+    }
+    
 }
