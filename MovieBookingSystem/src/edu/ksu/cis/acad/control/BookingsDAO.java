d5b2290738f678e57b76cde6e79adae0dd70f18c
Yusuf <yusufali2205@gmail.com>
2015-05-06 21:58:33 -0500

    bug fix

--- MovieBookingSystem/src/edu/ksu/cis/acad/control/BookingsDAO.java
+++ MovieBookingSystem/src/edu/ksu/cis/acad/control/BookingsDAO.java
@@ -48,12 +48,14 @@ public class BookingsDAO {
             
             // insert into bookings table
             String insert_booking_query = "INSERT INTO BOOKINGS "
-            				+ "VALUES (?, ?, ?, ?, ?)";
+            				+ "VALUES (?, ?, ?, ?, ?, ?)";
             PreparedStatement book_ticket_ps = dbConn.prepareStatement(insert_booking_query);
             book_ticket_ps.setString(1, "");
             book_ticket_ps.setString(2, booking.getUsername());
             book_ticket_ps.setInt(3, booking.getMovie_id());
             book_ticket_ps.setInt(4, booking.getTheatre_id());
+            book_ticket_ps.setString(5, booking.getShow_time());
+            book_ticket_ps.setDate(6, booking.getDate());
             
             int booking_done = book_ticket_ps.executeUpdate();
             
